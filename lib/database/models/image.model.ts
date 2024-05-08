11df48ffef2dfd3fd13e5703368dbb5bc8a818db
JanSekne <sekne50@gmail.com>
2024-05-08 13:14:21 +0200

    initial commit

---
+++ lib/database/models/image.model.ts
@@ -0,0 +1,44 @@
+import { transformationTypes } from "../../../../../constants/index";
+import { Document, Schema, model, models } from "mongoose";
+
+export interface IImage extends Document {
+  title: string;
+  transformationType: string;
+  publicId: string;
+  secureUrl: string;
+  width?: number;
+  height?: number;
+  config?: object;
+  tranformationUrl?: string;
+  color?: string;
+  aspectRatio?: string;
+  prompt?: string;
+  author?: {
+    _id: string;
+    firstName: string;
+    lastName: string;
+  };
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+const ImageSchema = new Schema({
+  title: { type: String, required: true },
+  transformationType: { type: String, required: true },
+  publicId: { type: String, required: true },
+  secureUrl: { type: URL, required: true },
+  width: { type: Number },
+  height: { type: Number },
+  config: { type: Object },
+  tranformationUrl: { type: URL },
+  color: { type: URL },
+  aspectRation: { type: String },
+  prompt: { type: String },
+  author: { type: Schema.Types.ObjectId, ref: "User" },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+});
+
+const Image = models?.Image || model("Image", ImageSchema);
+
+export default Image;
